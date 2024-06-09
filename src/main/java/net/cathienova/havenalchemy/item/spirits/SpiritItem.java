3758f3b24bdda444a9570c0b07f11ca4b453368c
Cathienna <Cathienna69@gmail.com>
2024-06-09 23:05:15 +0200

    Fixed a null exception with Essentia Farmland.

--- src/main/java/net/cathienova/havenalchemy/item/spirits/SpiritItem.java
+++ src/main/java/net/cathienova/havenalchemy/item/spirits/SpiritItem.java
@@ -1,6 +1,10 @@
 package net.cathienova.havenalchemy.item.spirits;
 
 import net.cathienova.havenalchemy.block.ModBlocks;
+import net.cathienova.havenalchemy.block.farmland.CelestiumFarmland;
+import net.cathienova.havenalchemy.block.farmland.EssentiaFarmland;
+import net.cathienova.havenalchemy.block.farmland.MysteriumFarmland;
+import net.cathienova.havenalchemy.block.farmland.VitaliumFarmland;
 import net.cathienova.havenalchemy.item.ModItems;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -46,37 +50,7 @@ public class SpiritItem extends Item
 
             return super.useOn(context);
         }
-        else if (block == ModBlocks.essentia_farmland.get() && context.getItemInHand().getItem() == ModItems.celestium_spirit.get())
-        {
-            if (!level.isClientSide())
-            {
-                level.setBlockAndUpdate(context.getClickedPos(), ModBlocks.celestium_farmland.get().defaultBlockState());
-                if (!player.isCreative())
-                    context.getItemInHand().shrink(1);
-            }
-            else
-            {
-                level.playSound(player, context.getClickedPos(), SoundEvents.ROOTED_DIRT_PLACE, SoundSource.BLOCKS, 0.5f, 1f);
-            }
-
-            return super.useOn(context);
-        }
-        else if (block == ModBlocks.celestium_farmland.get() && context.getItemInHand().getItem() == ModItems.eternium_spirit.get())
-        {
-            if (!level.isClientSide())
-            {
-                level.setBlockAndUpdate(context.getClickedPos(), ModBlocks.eternium_farmland.get().defaultBlockState());
-                if (!player.isCreative())
-                    context.getItemInHand().shrink(1);
-            }
-            else
-            {
-                level.playSound(player, context.getClickedPos(), SoundEvents.ROOTED_DIRT_PLACE, SoundSource.BLOCKS, 0.5f, 1f);
-            }
-
-            return super.useOn(context);
-        }
-        else if (block == ModBlocks.eternium_farmland.get() && context.getItemInHand().getItem() == ModItems.mysterium_spirit.get())
+        else if (block instanceof EssentiaFarmland && context.getItemInHand().getItem() == ModItems.mysterium_spirit.get())
         {
             if (!level.isClientSide())
             {
@@ -88,9 +62,47 @@ public class SpiritItem extends Item
             {
                 level.playSound(player, context.getClickedPos(), SoundEvents.ROOTED_DIRT_PLACE, SoundSource.BLOCKS, 0.5f, 1f);
             }
-
-            return super.useOn(context);
         }
+        else if (block instanceof MysteriumFarmland && context.getItemInHand().getItem() == ModItems.vitalium_spirit.get())
+        {
+            if (!level.isClientSide())
+            {
+                level.setBlockAndUpdate(context.getClickedPos(), ModBlocks.vitalium_farmland.get().defaultBlockState());
+                if (!player.isCreative())
+                    context.getItemInHand().shrink(1);
+            }
+            else
+            {
+                level.playSound(player, context.getClickedPos(), SoundEvents.ROOTED_DIRT_PLACE, SoundSource.BLOCKS, 0.5f, 1f);
+            }
+        }
+        else if (block instanceof VitaliumFarmland && context.getItemInHand().getItem() == ModItems.celestium_spirit.get())
+        {
+            if (!level.isClientSide())
+            {
+                level.setBlockAndUpdate(context.getClickedPos(), ModBlocks.celestium_farmland.get().defaultBlockState());
+                if (!player.isCreative())
+                    context.getItemInHand().shrink(1);
+            }
+            else
+            {
+                level.playSound(player, context.getClickedPos(), SoundEvents.ROOTED_DIRT_PLACE, SoundSource.BLOCKS, 0.5f, 1f);
+            }
+        }
+        else if (block instanceof CelestiumFarmland && context.getItemInHand().getItem() == ModItems.eternium_spirit.get())
+        {
+            if (!level.isClientSide())
+            {
+                level.setBlockAndUpdate(context.getClickedPos(), ModBlocks.eternium_farmland.get().defaultBlockState());
+                if (!player.isCreative())
+                    context.getItemInHand().shrink(1);
+            }
+            else
+            {
+                level.playSound(player, context.getClickedPos(), SoundEvents.ROOTED_DIRT_PLACE, SoundSource.BLOCKS, 0.5f, 1f);
+            }
+        }
+
         return InteractionResult.PASS;
     }
 }
